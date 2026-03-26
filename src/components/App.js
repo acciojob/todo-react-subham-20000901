314bdcaa42d16e7a7cef3d41f196f57e2b08d719
subham-20000901 <sinhasubh2000@gmail.com>
2026-03-26 20:32:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-
+import {useState} from 'react';
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
+  const [task,setTask] = useState('');
+  const [todos,setTodos] = useState([]);
+  function addTodo(){
+    setTodos([...todos,task]);
+    setTask("");
+  }
+    return (
     <div>
         {/* Do not remove the main div */}
+    <input 
+      type="text" 
+      placeholder="Entera task"
+      onChange = {(e) => setTask(e.target.value)} 
+        />
+    <button onClick={addTodo}>Add Todo</button>
+    <ol>
+      {todos.map((todo,index) => {
+        return (
+          <li key={index}>{todo}</li>
+        )
+      })}   
+    </ol>
     </div>
   )
 }
